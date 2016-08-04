1bb6362048b7b09c3953615c075c60376d9127fa
Firas Kudsy <Firas Kudsy@FIRASKUDSY>
2016-08-04 12:44:20 -0400

    USerPhoto added to registeration Model, Controll and View

---
+++ ScheduMate/Migrations/201608041551406_USerProfileAdded.cs
@@ -0,0 +1,18 @@
+namespace ScheduMate.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class USerProfileAdded : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "UserPhoto", c => c.Binary());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "UserPhoto");
+        }
+    }
+}
--- ScheduMate/Properties/AssemblyInfo.cs
+++ ScheduMate/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2016.8.4.17")]
-[assembly: AssemblyFileVersion("2016.8.4.17")]
+[assembly: AssemblyVersion("2016.8.4.29")]
+[assembly: AssemblyFileVersion("2016.8.4.29")]
