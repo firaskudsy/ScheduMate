f6fec8a06689b86104d0ff7a7e62f2ae4e48bd7f
Firas Kudsy <Firas Kudsy@FIRASKUDSY>
2016-08-04 10:49:24 -0400

    Department Class Created

---
+++ ScheduMate/Models/Departments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheduMate.Models
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public string Department { get; set; }
+
+    }
+}
